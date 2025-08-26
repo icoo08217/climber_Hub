ca87a703c08bc153a3e48646c519bead963b4628
byeongchan.jeon <byeongchan.jeon@concentrix.com>
2025-08-26 16:57:10 +0900

    postType Enum 타입 추가

---
+++ src/main/java/com/climbers/hub/post/constant/PostType.java
@@ -0,0 +1,8 @@
+package com.climbers.hub.post.constant;
+
+public enum PostType {
+    GYM_NOTICE, // 암장 공지사항
+    GYM_POST, // 암장 일반 게시글
+    GLOBAL_NOTICE, // 전체 공지사항
+    GLOBAL_POST, // 전체 일반 게시글
+}
