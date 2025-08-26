1785cdc0a2496701277a1a73d2b8f0afbcd0a837
byeongchan.jeon <byeongchan.jeon@concentrix.com>
2025-08-26 16:57:29 +0900

    findByEmail 메소드 추가

--- src/main/java/com/climbers/hub/member/repository/MemberRepository.java
+++ src/main/java/com/climbers/hub/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.climbers.hub.member.repository;
 import com.climbers.hub.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
